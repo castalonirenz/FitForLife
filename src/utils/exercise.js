0102ac1e5470b11a21309c7f80801623459c6692
Renz Castaloni <castalonirenz@gmail.com>
2019-09-13 08:42:27 +0800

    renamed

--- src/utils/exercise.js
+++ src/utils/exercise.js
@@ -1,7 +1,34 @@
 export const exercise = [
     {
         type: "Abdominal",
-        image: require('../assets/exercise/Abdominal/LegRaise.jpg')
+        image: require('../assets/exercise/Abdominal/LegRaise.jpg'),
+       data = [
+            {
+                name: "Ball Pull In",
+                image: null,
+                procedure: [
+                                "1.	Place an exercise ball nearby and lay on the floor in front of it with your hands on the floor shoulder width apart in a push-up position.",
+                                "2.	Now place your lower shins on top of an exercise ball. Tip: At this point your legs should be fully extended with the shins on top of the ball and the upper body should be in a push-up type of position being supported by your two extended arms in front of you. This will be your starting position.",
+                                "3.	While keeping your back completely straight and the upper body stationary, pull your knees in towards your chest as you exhale, allowing the ball to roll forward under your ankles. Squeeze your abs and hold that position for a second.",
+                                "4.	Now slowly straighten your legs, rolling the ball back to the starting position as you inhale.",
+                                "5.	Repeat for the recommended amount of repetitions."
+
+                ]
+            },
+            {
+                name: "Cross Body Crunch",
+                image: null,
+                procedure:[
+                    "1.	Lie flat on your back and bend your knees about 60 degrees.",
+                    "2.	Keep your feet flat on the floor and place your hands loosely behind your head. This will be your starting position.",
+                    "3.	Now curl up and bring your right elbow and shoulder across your body while bring your left knee in toward your left shoulder at the same time. Reach with your elbow and try to touch your knee. Exhale as you perform this movement. Tip: Try to bring your shoulder up towards your knee rather than just your elbow and remember that the key is to contract the abs as you perform the movement; not just to move the elbow.",
+                    "4.	Now go back down to the starting position as you inhale and repeat with the left elbow and the right knee.",
+                    "5.	Continue alternating in this manner until all prescribed repetitions are done.",
+                    "Variation: You can also do all of your repetitions for one side and then switch to the other side."
+
+                ]
+            }
+        ]
     },
     {
         type: "Back",
@@ -19,5 +46,29 @@ export const exercise = [
     {
         type: "Chest",
         image: require('../assets/exercise/Chest/Butterfly.jpg')
+    },
+    {
+        type: "Harmstring",
+        image: require('../assets/exercise/Harmstring/RomanianDeadlift.jpg')
+    },
+    {
+        type: "Oblique",
+        image: require('../assets/exercise/Oblique/DumbbellSideBend.jpg')
+    }
+    ,
+    {
+        type: "Quadriceps",
+        image: require('../assets/exercise/Quadriceps/BarbellLunge.jpg')
+    }
+    ,
+    {
+        type: "Shoulder",
+        image: require('../assets/exercise/Shoulder/BarbellPressBehindBack.jpg')
+    }
+    ,
+    {
+        type: "Triceps",
+        image: require('../assets/exercise/Triceps/Dips.jpg')
     }
 ]
+

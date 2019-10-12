6553aebd3e06f449d41c46d3a3d89dc8172f9d22
devB <devb@devBs-MacBook-Pro.local>
2019-10-12 21:16:10 +0800

    test

--- src/redux/actions/Auth.js
+++ src/redux/actions/Auth.js
@@ -1,7 +1,7 @@
 import { LOGIN, LOGOUT } from "../actionType";
 import Axios from 'axios'
 import { apiUrl, localserver } from "../../utils/url";
-import moment from 'moment'
+import moment, { duration } from 'moment'
 export const Auth = (credentials) => {
     
     return dispatch => {
@@ -10,32 +10,18 @@ export const Auth = (credentials) => {
             password: credentials.password
         })
         .then((response => {
-        //   console.log(response.data.data.first_time)
             if(response.data.status !== "Invalid Credentials"){
-                let sssss = "27-18-2021"
                 let expDate = response.data.data.cust_expdate
-                let expDateYear = moment(expDate).format('YYYY')
-                let expDateDay = moment(expDate).format('DD')
-                let expDateMonth = moment(expDate).format('MM')
-                let todayYear = moment().format('YYYY')
-                let todayDay = moment().format('DD')
-                let todayMonth = moment().format('MM')
-                let expTime = moment(expDate).format('hh:mm:ss a')
-                let todayTime = moment().format('hh:mm:ss a')
-                var a = moment([todayYear, todayMonth,todayDay, todayTime])
-                var b = moment([expDateYear, expDateMonth,expDateDay, expTime])
-                // var a = moment(expDate).format('LLLL')
-                // var b = moment().format('LLLL')
-                var c = moment().format('MM/DD/YYYY hh:mm:ss A')
-                // console.log(moment.utc(moment(expTime, "DD/MM/YYYY HH:mm:ss").diff(moment(todayTime, "DD/MM/YYYY HH:mm:ss"))))
-             
+              
                 let firstTime = response.data.data.first_time !== undefined ? response.data.data.first_time : 1
-                let diff = a.diff(b, 'minutes')
-               
-          
-                let add = diff
-                let minutes = moment.duration(add, 'minutes')
-                if(  minutes >= 180000){
+                // let diff = expTime.diff(todayTime, 'minutes')
+                let momomentExpDate = moment(expDate)
+                let today = moment()
+                console.log(moment.duration(today.diff(momomentExpDate)).get('minutes'), "--> where?")
+                let expDurationMinutes = moment.duration(today.diff(momomentExpDate)).get('minutes')
+                console.log(expDurationMinutes)
+                console.log(momomentExpDate.isSameOrAfter(today))
+                if (!momomentExpDate.isSameOrAfter(today)){
                   
                     return 'expired'
                 }
